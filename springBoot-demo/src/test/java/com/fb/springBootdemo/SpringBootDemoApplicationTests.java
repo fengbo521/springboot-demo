f8e3d9e65dec18cecf4065319129a019d7ade4a2
fb <fb@qq.com>
2020-01-06 09:18:53 +0800

    project init

---
+++ springBoot-demo/src/test/java/com/fb/springBootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb.springBootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
